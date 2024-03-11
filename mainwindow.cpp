76e5be7e31739ed32927dde6ac276ec68a0f1294
Vladislav <vlad.b8239@gmail.com>
2024-03-11 18:45:55 +0300

    Added database connection

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,58 +1,99 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include <mysql.h>
-struct connection_details
-{
-    const char *server, *user, *password, *database;
-};
+#include <mainwindow.h>
+#include <ui_mainwindow.h>
 
-MYSQL* mysql_connection_setup(struct connection_details mysql_details){
-    MYSQL *connection = mysql_init(NULL); // mysql instance
+DataBase::DataBase() {};
 
-    //connect database
-    if(!mysql_real_connect(connection, mysql_details.server, mysql_details.user, mysql_details.password, mysql_details.database, 0, NULL, 0)){
-        exit(1);
-    }
-
-    return connection;
+DataBase::DataBase(const char* serverIP, const char* user, const char* passwd, const char*dbName) {
+    connection = mysql_init(nullptr);
+    mysql_real_connect(connection, serverIP, user, passwd, dbName, 0, NULL, 0);
 }
 
-// mysql_res = mysql result
-MYSQL_RES* mysql_perform_query(MYSQL *connection, const char *sql_query){
-    //send query to db
-    if(mysql_query(connection, sql_query)){
-        exit(1);
+DataBase::DataBase(const DataBase& copy) {
+    if (copy.connection != nullptr) {
+        connection = mysql_init(nullptr);
+        mysql_real_connect(connection, copy.connection->host, copy.connection->user, copy.connection->passwd, copy.connection->db, 0, NULL, 0);
     }
+}
 
+DataBase& DataBase::operator=(const DataBase& copy) {
+    if (this != &copy) {
+        delete connection;
+        if (copy.connection != nullptr) {
+            connection = mysql_init(nullptr);
+            mysql_real_connect(connection, copy.connection->host, copy.connection->user, copy.connection->passwd, copy.connection->db, 0, NULL, 0);
+        }
+    }
+    return *this;
+}
+
+void DataBase::setNewConnection(char* serverIP, char* user, char* passwd, char*dbName) {
+    mysql_close(connection);
+    connection = mysql_init(nullptr);
+    mysql_real_connect(connection, serverIP, user, passwd, dbName, 0, NULL, 0);
+}
+
+MYSQL_RES* DataBase::performQuery(const char *query) {
+    mysql_query(connection, query);
     return mysql_use_result(connection);
 }
+
+bool DataBase::isConnectionValid() {
+    if (connection != nullptr) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+DataBase::~DataBase() {
+    mysql_close(connection);
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
-{
+    , stringValidator(new QRegularExpressionValidator(QRegularExpression("[А-я]{1,20}"),this)) {
     ui->setupUi(this);
-    dbWindow = new DBWindow();
-    connect(dbWindow, &DBWindow::firstWindow, this, &MainWindow::show);
+    hideAll();
+    ui->departureCityLine->setValidator(stringValidator);
+    ui->arrivalCityLine->setValidator(stringValidator);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
+    delete stringValidator;
     delete ui;
 }
 
-void MainWindow::on_buttonSearch_clicked()
-{
-
+void MainWindow::on_buttonSearch_clicked() {
+    ui->buttonSearch->setText(QString::number(ui->buttonSearch->sizeHint().height()));
 }
 
-void MainWindow::on_actionDB_triggered()
-{
-    dbWindow->show();
-    //this->close();
+void MainWindow::hideAll() {
+    ui->departureCityList->setVisible(false);
+    ui->arrivalCityList->setVisible(false);
+    ui->dateCalendar->setVisible(false);
+    ui->oneWayButton->setVisible(false);
 }
 
-void MainWindow::on_actionExit_triggered()
-{
-    close();
+void MainWindow::on_departureDateButton_clicked() {
+    hideAll();
+    ui->dateCalendar->setVisible(true);
 }
 
+void MainWindow::on_backDateButton_clicked() {
+    hideAll();
+    ui->dateCalendar->setVisible(true);
+    ui->oneWayButton->setVisible(true);
+}
+
+void MainWindow::on_dateCalendar_clicked(const QDate &date) {
+    hideAll();
+}
+
+void MainWindow::on_menuGroupBox_clicked() {
+    hideAll();
+}
+
+void MainWindow::on_changeButton_clicked() {
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <dbwindow.h>
+#include <QMessageBox>
+#include <QValidator>
+
+#include <mysql.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -10,23 +13,53 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
 private slots:
     void on_buttonSearch_clicked();
 
-    void on_actionDB_triggered();
+    void on_departureDateButton_clicked();
 
-    void on_actionExit_triggered();
+    void on_dateCalendar_clicked(const QDate &date);
+
+    void on_backDateButton_clicked();
+
+    void on_menuGroupBox_clicked();
+
+    void on_changeButton_clicked();
 
 private:
+    void hideAll();
+
     Ui::MainWindow *ui;
-    DBWindow* dbWindow;
+    QRegularExpressionValidator* stringValidator;
+};
+
+class DataBase {
+public:
+    DataBase();
+
+    DataBase(const char* serverIP, const char* user, const char* passwd, const char*dbName);
+
+    DataBase(const DataBase& copy);
+
+    DataBase& operator=(const DataBase& copy);
+
+    void setNewConnection(char* serverIP, char* user, char* passwd, char*dbName);
+
+    MYSQL_RES* performQuery(const char *query);
+
+    bool isConnectionValid();
+
+    ~DataBase();
+
+private:
+    MYSQL *connection = nullptr;
 };
 #endif // MAINWINDOW_H
